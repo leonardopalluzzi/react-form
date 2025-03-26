c4a6e14b0debd82c2c64dfb11394d8d9138fc186
Leonardo Palluzzi <lp.palluzzi@gmail.com>
2025-03-26 22:59:13 +0100

    Refactor Input component form styling and remove unnecessary CSS class

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,13 +1,13 @@
 export default function Input({ newArticle, setNewArticle, handleSubmit }) {
     return (
         <div className="container">
-            <form id='insert_form' onSubmit={e => handleSubmit(e, newArticle)} action="">
+            <form className='insert_form d-flex flex-column my-5' onSubmit={e => handleSubmit(e, newArticle)} action="">
                 <input
                     className="col-8"
                     onChange={(e) => setNewArticle(e.target.value)}
                     value={newArticle} type="text"
                     placeholder="Insert the article's title" />
-                <button type="submit" className="col-2 btn btn-primary mx-4">Add</button>
+                <button type="submit" className="col-2 btn btn-primary my-3">Add</button>
             </form>
         </div>
     )
